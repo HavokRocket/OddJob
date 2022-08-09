9e98902e94e75a4fb5efe68042a84d4538d80749
Erik Bermudez <unmillon16@gmail.com>
2022-08-09 15:33:02 -0500

    update 1

--- script.js
+++ script.js
@@ -1,11 +1,13 @@
-let Listings = []
-
 class listing{
-    constructor(length, header, img){
+    constructor(length, header, img, paidorvolunteer, summary){
         let distance = length;
         let title = header;
         let image = img;
-
-        
+        let type = paidorvolunteer;
+        let description = summary;
     }
 }
+
+particlesJS.load('main', 'particles.json', function() {
+    console.log('callback - particles.js config loaded');
+  });
