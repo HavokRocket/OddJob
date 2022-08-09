f68634f3dc36930043cafd1bd8cf360cb0f2944e
Erik Bermudez <unmillon16@gmail.com>
2022-08-09 11:04:52 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,11 @@
+let Listings = []
+
+class listing{
+    constructor(length, header, img){
+        let distance = length;
+        let title = header;
+        let image = img;
+
+        
+    }
+}
